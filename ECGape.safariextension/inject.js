a4f8e5cfe45601a9c3ee37a241d29e23dad63fec
dvdrtrgn <dvdrtrgn@gmail.com>
2011-01-13 09:59:50 -0600

    1.0.2\ simplify js, tweak css, b/w list

--- ECGape.safariextension/inject.js
+++ ECGape.safariextension/inject.js
@@ -1,8 +1,6 @@
-var players = {};
+var injectOr = function(event) {};
 
 document.addEventListener('beforeload', function(event) { }, true);
-
-var injectOr = function(event) {};
 
 safari.self.addEventListener("message", function(event) {
     if (event.name === "injectOr") {
