a3e02700ba92bff3014fb2b248a0fd0494fffece
dvdrtrgn <dvdrtrgn@gmail.com>
2011-01-15 22:19:58 -0600

    first commit

---
+++ Yahpaz.safariextension/inject.js
@@ -0,0 +1,11 @@
+var injectOr = function(event) {};
+
+document.addEventListener('beforeload', function(event) { }, true);
+
+safari.self.addEventListener("message", function(event) {
+    if (event.name === "injectOr") {
+        injectOr(event);
+    }
+}, true);
+
+window.status = 'Yahpaz does not do much yet';
